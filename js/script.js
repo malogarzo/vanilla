3db62d0db91b395d9803875786d90c41202e0ac5
Logarzo <mariano.logarzo@gmail.com>
2018-08-29 21:52:36 -0300

    error handling

--- js/script.js
+++ js/script.js
@@ -18,7 +18,12 @@
 	async function getData(url) {
   	const response = await fetch(url);
   	const data = await response.json();
-  	return data;
+    if (data.data.movie_count > 0) {
+
+      return data;
+    }
+    
+    throw new Error('No se encontró ningún resultado');
   }
   const $form = document.getElementById('form');
   const $home = document.getElementById('home');
@@ -61,14 +66,20 @@
     $featuringContainer.append($loader);
 
     const data = new FormData($form);
-    const {
-      data: {
-        movies: mov
-      }
-    } = await getData(`${BASE_API}list_movies.json?limit=1&query_term=${data.get('name')}`)
+    try {
+      const {
+        data: {
+          movies: mov
+        }
+      } = await getData(`${BASE_API}list_movies.json?limit=1&query_term=${data.get('name')}`)
     
-    const HTMLString = featuringTemplate(mov[0]);
-    $featuringContainer.innerHTML = HTMLString;
+      const HTMLString = featuringTemplate(mov[0]);
+      $featuringContainer.innerHTML = HTMLString;
+    } catch(error) {
+      alert(error.message);
+      $loader.remove();
+      $home.classList.remove('search-active');
+    }
   })
 
   function videoItemTemplate(movie, category) {
